79e25e2ca25719b20e513cbed1d34b4b731c4494
Satish <satishpanjala3@gmail.com>
2026-07-14 18:02:01 +0530

    Update UserController.java

--- src/main/java/com/satish/expense_tracker/controller/UserController.java
+++ src/main/java/com/satish/expense_tracker/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://expense-tracker-gamma-rouge.vercel.app")
 public class UserController {
 
     private final UserService userService;
