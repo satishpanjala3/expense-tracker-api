c3a090b044c4dddcd40cf9cfea8dffea124fe2b1
Satish <satishpanjala3@gmail.com>
2026-07-14 18:20:11 +0530

    Update ProfileController.java

--- src/main/java/com/satish/expense_tracker/controller/ProfileController.java
+++ src/main/java/com/satish/expense_tracker/controller/ProfileController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/profile")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://expense-tracker-gamma-rouge.vercel.app")
 public class ProfileController {
     private final ProfileService profileService;
 
