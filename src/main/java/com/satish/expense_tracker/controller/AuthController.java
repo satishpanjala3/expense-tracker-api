1e1e9e57ef9e78ec7464cadebc143527f92e6a4e
Satish <satishpanjala3@gmail.com>
2026-07-14 18:18:57 +0530

    Update AuthController.java

--- src/main/java/com/satish/expense_tracker/controller/AuthController.java
+++ src/main/java/com/satish/expense_tracker/controller/AuthController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://expense-tracker-gamma-rouge.vercel.app")
 public class AuthController {
 
     private final AuthService authService;
